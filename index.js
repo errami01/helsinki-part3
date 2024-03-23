82fcfb8f415b5961ce83ee527f5843defa9117f1
Abdellatif ERRAMI <abdellatif.errami@gmail.com>
2024-03-23 18:35:17 +0000

    Add exercice 3.14 phonebook database step 2

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
 const express = require('express')
 require('dotenv').config()
 const morgan = require('morgan')
-const mongoose = require('mongoose')
-
+const Person = require('./models/person')
 
 const app = express()
 app.use(express.json())
 app.use(express.static('dist'))
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :req[content-length] - :response-time ms :body'))
-const url = process.env.MONGO_URI
-mongoose.connect(url)
-const personSchema = mongoose.Schema({
-  name : String,
-  number: String
-})
-const Person = mongoose.model('Person', personSchema)
+
 let persons =[
     { 
       "id": 1,
@@ -62,33 +55,34 @@ app.get('/api/persons/:id',(req, res)=>{
     .then(person=>res.json(person))
     .catch(()=>res.status(404).end())
 })
-app.delete('/api/persons/:id',(req, res)=>{
-    const id = Number(req.params.id)
-    persons = persons.filter((person)=> person.id !== id)
-    res.status(204).end
-})
+// app.delete('/api/persons/:id',(req, res)=>{
+//     const id = Number(req.params.id)
+//     persons = persons.filter((person)=> person.id !== id)
+//     res.status(204).end
+// })
 app.post('/api/persons', (req, res)=>{
-    const id = Math.floor(Math.random()*1000)
+    // const id = Math.floor(Math.random()*1000)
     const body = req.body
     if(!body.name || !body.number) {
         return res.status(400).json({ 
             error: 'name or number missing' 
         })
     }
-    if(persons.some((person)=> person.name.toLowerCase() === body.name.toLowerCase())){
-        return res.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-    const person = {
+    // if(persons.some((person)=> person.name.toLowerCase() === body.name.toLowerCase())){
+    //     return res.status(400).json({
+    //         error: 'name must be unique'
+    //     })
+    // }
+    const person = new Person({
         name: body.name,
         number: body.number,
-        id: id
-    }
-    persons = persons.concat(person)
-    res.json(person)
+    })
+    person
+      .save()
+      .then(savedPerson=>res.json(savedPerson))
+    
 })
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
